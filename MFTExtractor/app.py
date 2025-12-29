c2db73cbe4c216cd0a703115fec4e03f793a6cfe
José Ramón López <jose.rlg27@gmail.com>
2025-12-29 21:18:55 +0100

    Complete the main method & add the method for writing the binary output file with the content of the MFT.

--- MFTExtractor/app.py
+++ MFTExtractor/app.py
@@ -1,8 +1,8 @@
 """
-MFTExtractor v1.0
-MFTExtractor - Tool for reading and extracting the Master File Table (MFT) in an NTFS file system.
-Author: José Ramón López Guillén
-GitHub: github.com/Sysop81
+    MFTExtractor v1.0
+    MFTExtractor - Tool for reading and extracting the Master File Table (MFT) in an NTFS file system.
+    Author: José Ramón López Guillén
+    GitHub: github.com/Sysop81
 
 """
 
@@ -10,9 +10,11 @@ from display.dhandler import Display
 from params.params_handler import Parameters
 from helpers.privileges import Privileges
 from winapi.volume_reader import VolumeReader
+from helpers.tools import Utils
 
 def main():
     Display.show_banner()
+    
     # Get params and check admin privileges
     params = Parameters() 
     Privileges.check_privileges(params.get_command_line_args())    
@@ -21,5 +23,13 @@ def main():
     ntfs_volume = VolumeReader(params.get_input())
     ntfs_volume.get_ntfs_info()
     ntfs_volume.set_file_pointer()
+    ntfs_volume.read_mft_bytes()
+
+    # build binary output file
+    Display.show_info("Building the output file")
+    Utils.build_output_file(
+        params.get_output(),
+        ntfs_volume.complete_mft_buffer
+    )
     
     Display.show_info("End program")    
--- MFTExtractor/helpers/tools.py
+++ MFTExtractor/helpers/tools.py
@@ -1,4 +1,5 @@
 import re
+from pathlib import Path
 
 class Utils:
 
@@ -9,11 +10,13 @@ class Utils:
         er = r"^[A-Z]:?\\?$"
         return bool(re.match(er,volume.strip(),re.IGNORECASE))
     
+    @staticmethod
     def get_volume_letter(volume : str) -> str:
         er = r"^([A-Z])"
         mvalue = re.match(er,volume.strip(),re.IGNORECASE)
         return mvalue.group(1).upper()
     
+    @staticmethod
     def is_correct_file_name(file_name : str) -> bool:
         file_name = file_name.strip()
         base_name = file_name.split(".")[0]
@@ -21,8 +24,21 @@ class Utils:
         er = r"[A-Za-z_]+"
         return bool(re.fullmatch(er,base_name))
     
+    @staticmethod
     def get_output_file_name(file_name : str) -> str:
         file_name = file_name.strip()
         base_name = file_name.split(".")[0]
 
         return f"{base_name}.bin" 
+    
+    # FileWriter
+
+    @staticmethod
+    def build_output_file(filename : str, mft_data : bytes):
+        output_folder = "output"
+        output_path = Path(__file__).resolve().parents[1] / output_folder
+        output_path.mkdir(parents=True, exist_ok=True)
+        complete_path = output_path / filename
+
+        with open(complete_path, "wb") as f:
+            f.write(mft_data)
